f933fca6b9ec126ebb1254d83ec76ebf3d6b1d3d
Your Name <ag.salima91@gmail.com>
2023-11-29 14:25:55 +0100

    adding sal branch

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -3,3 +3,4 @@
 <p>Merge change</p>
 <p>Merge modicfication</p>
 <p>Merge second modification</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti saepe commodi in, aspernatur nam necessitatibus provident, sunt quidem ipsa maxime odit tenetur reprehenderit facere sint explicabo inventore recusandae assumenda.</p>
